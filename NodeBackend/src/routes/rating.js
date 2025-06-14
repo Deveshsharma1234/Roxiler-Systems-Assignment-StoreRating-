3030895a75ad86748e0352a86ad34fd08b91032b
Devesh Sharma <deveshsharma372002@gmail.com>
2025-06-15 04:24:53 +0530

    Update pass

--- NodeBackend/src/routes/rating.js
+++ NodeBackend/src/routes/rating.js
@@ -34,4 +34,37 @@ ratingRouter.post("/add-Rating", authAndAuthorize(1,2,3), (req, res) => {
 })
 
 
+// Update existing rating by user for a store
+ratingRouter.patch("/update-rating", authAndAuthorize(1,2,3), (req, res) => {
+    try {
+        const UserId = req.user?.UserId;
+ 
+        const { rating, comment,StoreId } = req.body;
+
+        if (!rating || rating < 1 || rating > 5) {
+            return res.status(400).json({ message: "Rating must be between 1 and 5." });
+        }
+
+        const updateQuery = `UPDATE ratings 
+            SET rating = ?, comment = ?, rated_at = CURRENT_TIMESTAMP
+            WHERE UserId = ? AND StoreId = ?
+        `;
+
+        db.pool.query(updateQuery, [rating, comment, UserId, StoreId], (err, result) => {
+            if (err) {
+                return res.status(500).json({ message: err.message });
+            }
+
+            if (result.affectedRows === 0) {
+                return res.status(404).json({ message: "Rating not found for this user and store." });
+            }
+
+            res.status(200).json({ message: "Rating updated successfully." });
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
 module.exports = ratingRouter;
--- NodeBackend/src/routes/user.js
+++ NodeBackend/src/routes/user.js
@@ -2,9 +2,11 @@ const express = require('express');
 const userRouter = express.Router();
 const db = require("../config/db");
 const authAndAuthorize = require('../middleware/authAndAuthorize');
+const CryptoJS = require('crypto-js');
+
 
 // GET all normal and admin users (roleId 1 and 2)
-userRouter.get("/all-user",authAndAuthorize(1), async (req, res) => {
+userRouter.get("/all-user", authAndAuthorize(1), async (req, res) => {
     try {
         const query = `
             SELECT 
@@ -29,4 +31,23 @@ userRouter.get("/all-user",authAndAuthorize(1), async (req, res) => {
     }
 });
 
+
+userRouter.patch("/update-password", authAndAuthorize(1, 2, 3), (req, res) => {
+    try {
+        const UserId = req.user?.UserId;
+        const { Password } = req.body;
+        const hashedPassword = CryptoJS.SHA256(Password).toString(CryptoJS.enc.Base64);
+        const statement = `update users set Password = ? where UserId = ?`;
+        db.pool.execute(statement, [hashedPassword, UserId], (err, result) => {
+            if (err) {
+                return res.status(500).json({ message: err.message });
+            }
+
+            return res.status(200).json({ message: "Password updated successfully." });
+
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
 module.exports = userRouter;
