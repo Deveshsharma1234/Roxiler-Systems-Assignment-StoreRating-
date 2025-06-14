2e8f8d5f353e461fa57cf1754265f5b9a240b39f
Devesh Sharma <deveshsharma372002@gmail.com>
2025-06-14 23:44:57 +0530

    db pool connection

--- NodeBackend/src/app.js
+++ NodeBackend/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { connectDB } = require("./config/db")
 const cookieParser = require('cookie-parser')
+const cors = require('cors');
 const app = express();
 
 
@@ -7,14 +9,24 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}))
 
 
-app.use("/ping",(req, res, next)=>{
+
+app.use("/");
+app.use("/ping", (req, res, next) => {
     res.send("Hello from server")
 
 })
 
-app.listen(3000 , ()=>{
-    console.log("Server started at port no 3000");
-    
+connectDB();
+
+
+
+app.listen(3000, () => {
+    console.log("Server started at port no 3000.");
+
 })
