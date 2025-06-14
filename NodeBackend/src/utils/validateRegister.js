337fdca82fd174d342374818c90153a20ee9d2c1
Devesh Sharma <deveshsharma372002@gmail.com>
2025-06-15 01:33:44 +0530

    User Login and admin registeration

--- NodeBackend/src/utils/validateRegister.js
+++ NodeBackend/src/utils/validateRegister.js
@@ -5,7 +5,7 @@ const validateRegister = (req) => {
     if (!FirstName || !LastName || !Email || !Phone  || !Password) throw new Error("All fields are required error from backend");
     if (!validate.isEmail(Email) || !validate.isStrongPassword(Password)) throw new Error("Invalid email or password");
     if(req.body.RoleId){
-        if(![1,3].includes(req.body.RoleId)) throw new Error("Invalid RoleId");
+        if(![1,2,3].includes(req.body.RoleId)) throw new Error("Invalid RoleId");
 
     }
 }
